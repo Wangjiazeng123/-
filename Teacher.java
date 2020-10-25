b76999b2ba6856dc9f717077ad389cda30ec965f
Wangjiazeng123 <72551867+Wangjiazeng123@users.noreply.github.com>
2020-10-25 18:55:08 +0800

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,41 @@
+package Information;
+
+import java.util.Scanner;
+
+
+public class Teacher extends People {
+	private String teacherName;
+	private String teacherId;
+	  ;
+public Teacher(int number,String name,String id,String course){
+	setNo(number);
+	setTeacherName(name);
+	setTeacherId(id);
+	setTeaCourse(course);
+}
+public int getNo(){
+	return num;	
+}
+void setNo(int number) {
+	this.num=number;	
+}
+public String getTeacherName(){
+	return teacherName;	
+}
+void setTeacherName(String teacherName) {
+	this.teacherName = teacherName;	
+}
+public String getTeacherId(){
+	return teacherId;	
+}
+void setTeacherId(String teacherId) {
+	this.teacherId = teacherId;	
+}
+public void setTeaCourse(String course) {
+	super.course=course;
+	}
+public String getTeaCourse() {
+	return super.course;
+	}
+
+}
